67a89d47d7b2f4fb14eb22b47a924ea6ad9903e9
Takuya Sugiyama (@motojouya) <motojouya@gmail.com>
2024-10-02 00:14:49 +0000

    install script

--- resources/init.sh
+++ resources/init.sh
@@ -1,10 +1,7 @@
 #!/bin/bash
 set -x
 
-# definitions
 ssh_port=$1
-
-# start configurations
 cd /home/ubuntu
 
 # sshd
@@ -23,73 +20,41 @@ chown -R ubuntu:ubuntu .
 
 # apt
 apt-get update
-apt-get install -y jq
-apt-get install -y tmux
-apt-get install -y tree
-apt-get install -y xauth
-apt-get install -y git
-apt-get install -y silversearcher-ag
-apt-get install -y sqlite3
+apt-get install -y jq tmux tree xauth git silversearcher-ag sqlite3 curl
 
 # node
 curl -fsSL https://deb.nodesource.com/setup_22.x | bash - && apt-get install -y nodejs
-npm install -g npx
-npm install -g typescript typescript-language-server
+npm install -g npx typescript typescript-language-server
 
 # litestream
 curl https://github.com/benbjohnson/litestream/releases/download/v0.3.13/litestream-v0.3.13-linux-amd64.deb -O
 dpkg -i litestream-v0.3.13-linux-amd64.deb
-
-# apt again
-apt-get install -y gnupg
-apt-get install -y software-properties-common
-
-# docker
-apt-get install -y \
-    ca-certificates \
-    curl \
-    gnupg
-mkdir -m 0755 -p /etc/apt/keyrings
-curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
-echo \
-  "deb [arch="$(dpkg --print-architecture)" signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu \
-  "$(. /etc/os-release && echo "$VERSION_CODENAME")" stable" | \
-  sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
-apt-get update
-apt-get install -y docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
-
-gpasswd -a ubuntu docker
-systemctl restart docker
-
-# curl -L https://github.com/docker/compose/releases/download/1.24.0/docker-compose-`uname -s`-`uname -m` -o /usr/local/bin/docker-compose
-# chmod +x /usr/local/bin/docker-compose
-
-# install docker
-dnf install -y docker
-systemctl enable --now docker
-systemctl status docker
-usermod -aG docker $username
-docker info
-
-sudo dnf install -y docker
-sudo systemctl enable --now docker
-docker info
-
-DOCKER_CONFIG=${DOCKER_CONFIG:-/usr/local/lib/docker}
-mkdir -p $DOCKER_CONFIG/cli-plugins
-curl -SL https://github.com/docker/compose/releases/download/v2.23.3/docker-compose-linux-x86_64 -o $DOCKER_CONFIG/cli-plugins/docker-compose
-chmod +x /usr/local/lib/docker/cli-plugins/docker-compose
-docker compose version
-# dnf config-manager --add-repo https://download.docker.com/linux/fedora/docker-ce.repo
-# dnf --releasever=36 install -y docker-buildx-plugin docker-compose-plugin
-# gpasswd -a $username docker
+# 
+# # apt again
+# apt-get install -y ca-certificates
+# apt-get install -y gnupg
+# apt-get install -y software-properties-common
+# 
+# # docker
+# mkdir -m 0755 -d /etc/apt/keyrings # install?
+# curl -fsSL https://download.docker.com/linux/ubuntu/gpg -o /etc/apt/keyrings/docker.asc
+# chmod a+r /etc/apt/keyrings/docker.asc
+# 
+# echo \
+#   "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/ubuntu \
+#   $(. /etc/os-release && echo "$VERSION_CODENAME") stable" | \
+#   tee /etc/apt/sources.list.d/docker.list > /dev/null
+# apt-get update
+# apt-get install -y docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+# 
+# gpasswd -a ubuntu docker
 # systemctl restart docker
-
-# terraform
-curl -fsSL https://apt.releases.hashicorp.com/gpg | \
-  gpg --dearmor -o /usr/share/keyrings/hashicorp-archive-keyring.gpg
-# gpg --no-default-keyring --keyring /usr/share/keyrings/hashicorp-archive-keyring.gpg --fingerprint
-echo "deb [signed-by=/usr/share/keyrings/hashicorp-archive-keyring.gpg] https://apt.releases.hashicorp.com $(lsb_release -cs) main" | \
-  tee /etc/apt/sources.list.d/hashicorp.list > /dev/null
-apt update
-apt-get install terraform
+# 
+# # terraform
+# curl -fsSL https://apt.releases.hashicorp.com/gpg | \
+#   gpg --dearmor -o /usr/share/keyrings/hashicorp-archive-keyring.gpg
+# # gpg --no-default-keyring --keyring /usr/share/keyrings/hashicorp-archive-keyring.gpg --fingerprint
+# echo "deb [signed-by=/usr/share/keyrings/hashicorp-archive-keyring.gpg] https://apt.releases.hashicorp.com $(lsb_release -cs) main" | \
+#   tee /etc/apt/sources.list.d/hashicorp.list > /dev/null
+# apt update
+# apt-get install terraform
